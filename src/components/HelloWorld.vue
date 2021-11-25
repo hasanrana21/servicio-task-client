6234eab9d3b820bc4a0ae8b81366d75e66e08689
hasanrana21 <hasanrana178821@gmail.com>
2021-11-25 17:50:34 +0600

    todo list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <form action="" @submit.prevent="handleSubmit">
+      <label for="title">Title</label> <br>
+      <input type="text" name="title" id="title" v-model="title" /> <br><br>
+
+      <label for="description">Description</label> <br>
+      <input type="text" name="description" id="description" v-model="description" /> <br><br>
+
+      <input type="submit" value="Submit">
+    </form>
+
+    <div>
+      <ul>
+        <li v-for="(list, index) in lists" :key="index">
+          <p>{{list.title}}</p>
+          <p> {{ list.description}}</p>
+          <button @click="editList(index)" :style="{margin: '0px 10px'}">Edit</button>
+          <button @click="deleteList">Delete</button>
+          <p> {{index}} </p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -35,6 +29,39 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      title: '',
+      description: '',
+      lists: [
+        {
+          title: 'apple',
+          description: 'lorem1asdkfjalskdjfadfkajsdf'
+        }
+      ]
+    }
+  },
+
+  methods:{
+    handleSubmit(){
+      this.lists.push({
+        title: this.title,
+        description: this.description,
+      })
+    },
+
+    deleteList(){
+      this.lists.pop();
+    },
+
+    editList(index){
+      // this.lists[index === ]
+      console.log(this.lists[index].title)
+      
+      this.lists[index].title
+
+    }
   }
 }
 </script>
