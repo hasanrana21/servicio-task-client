b44a4605d0bf3e37997e959bbfa5d78cf6034260
hasanrana21 <hasanrana178821@gmail.com>
2021-11-25 19:52:29 +0600

    show user in ui

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,21 +1,47 @@
 <template>
+  <div v-for="(user, index) in allUsers" :key="index" class="userInfo">
+    <h2>{{ user.title }}</h2>
+    <p>{{ user.description }}</p>
+    <p>{{user._id}}</p>
     <div>
-        <h1>this Users Page</h1>
+      <button>Edit</button>
+      <button @click="deleteUser(user._id)">Delete</button>
     </div>
+  </div>
 </template>
 
-
 <script>
+import axios from "axios";
+
 export default {
-    name: 'Users',
-    data(){
-        return{
+  name: "Users",
+  data() {
+    return {
+      allUsers: [],
+    };
+  },
 
-        }
+  mounted() {
+    axios
+      .get("http://localhost:5050/getUsers")
+      .then((res) => {
+        console.log(res.data);
+        this.allUsers = res.data;
+      })
+      .catch((err) => console.log(err));
+  },
+
+  methods: {
+    deleteUser(id) {
+    axios.delete('')
+      console.log(id)
     },
-
-    methods(){
-        
-    }
-}
+  },
+};
 </script>
+
+<style scoped>
+.userInfo button {
+  margin: 0 15px;
+}
+</style>
